4d9f75c473011a606cdd52157ba45ccd11853222
Madelyn Romero <m.romero013@gmail.com>
2019-12-19 09:37:07 -0700

    Create many to many relationship to machines

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -2,4 +2,9 @@ class Snack < ApplicationRecord
 
   validates_presence_of :name, :price
   validates_numericality_of :price
+
+  has_many :machine_snacks
+  has_many :machines, through: :machine_snacks
+
+
 end
