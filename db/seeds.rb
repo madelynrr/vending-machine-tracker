419f5aa3a12090cfb42611877408a0b415cd7aa1
Madelyn Romero <m.romero013@gmail.com>
2019-12-19 10:22:52 -0700

    Add to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 owner = Owner.create(name: "Sam's Snacks")
 dons  = owner.machines.create(location: "Don's Mixed Drinks")
+mikes  = owner.machines.create(location: "Mike's Place")
+
 
 snickers = Snack.create(name: "Snickers", price: 1)
 bbq = Snack.create(name: "BBQ Potato Chips", price: 2)
 mms = Snack.create(name: "M&Ms", price: 1)
 
-dons.snacks << [snickers, bbq, mms]
+dons.snacks << [snickers, bbq]
+mikes.snacks << [snickers, bbq, mms]
