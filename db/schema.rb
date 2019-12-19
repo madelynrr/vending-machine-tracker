e43e040a2b7d3345ade12b6b3c3af0b1ff788343
Madelyn Romero <m.romero013@gmail.com>
2019-12-19 09:36:52 -0700

    Create machine snack joins table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191219162128) do
+ActiveRecord::Schema.define(version: 20191219162944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "machine_snacks", force: :cascade do |t|
+    t.bigint "machine_id"
+    t.bigint "snack_id"
+    t.index ["machine_id"], name: "index_machine_snacks_on_machine_id"
+    t.index ["snack_id"], name: "index_machine_snacks_on_snack_id"
+  end
 
   create_table "machines", force: :cascade do |t|
     t.string "location"
@@ -32,5 +39,7 @@ ActiveRecord::Schema.define(version: 20191219162128) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "machine_snacks", "machines"
+  add_foreign_key "machine_snacks", "snacks"
   add_foreign_key "machines", "owners"
 end
